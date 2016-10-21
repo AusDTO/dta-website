daf829e280cd7543dda91dadc0a3795c23696661
Mick Pollard <aussielunix@gmail.com>
2016-10-21 15:38:36 +1100

    setup CD to cloud.gov.au

--- bin/citest.sh
+++ bin/citest.sh
@@ -10,24 +10,21 @@ set -e
 # cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
 set -o pipefail
 
-for file in $(dirname "$0")/../_posts/*; do
-    filename=$(basename "$file")
-    if ! [[ $filename =~ ^[a-z0-9\ -.]+$ ]]; then
-      echo "Bad filename: _posts/$filename"
-    fi
-done
-
 # echo out each line of the shell as it executes
 set -x
 
 # Run jekyll hyde
 bundle exec jekyll hyde
 
+# Check for url conflicts
+bundle exec jekyll doctor
+
+
 # Run a html proofer over the site
-#bundle exec htmlproofer _site  \
-#    --disable-external \
-#    --allow-hash-href \
-#    --url-ignore "/((#open-menu)|(#open-search)|(mailto:.*))/" \
-#    --file-ignore /.*feed/index\.html/ \
-#    --empty-alt-ignore
+bundle exec htmlproofer _site  \
+    --disable-external \
+    --allow-hash-href \
+    --url-ignore "/((#open-menu)|(#open-search)|(mailto:.*))/" \
+    --file-ignore /.*feed/index\.html/ \
+    --empty-alt-ignore
 
