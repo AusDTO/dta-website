619096122cc28d03c38873ef530f8d13ebc79f4c
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-11-24 15:53:36 +1100

    Combine js assets

---
+++ _assets/js/main-ie8.js
@@ -0,0 +1,7 @@
+
+// For IE 8 and below, this file should be loaded before main.js
+// to supply the dependencies for ui-kit.js
+
+//= require 'vendor/selectivizr-1.0.2-min'
+//= require 'vendor/respond-1.4.2.min'
+//= require 'vendor/html5shiv-3.7.3'
