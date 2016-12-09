84036b7ef7090134e6b4aae0edf176edf82ae26f
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-12-09 15:03:27 +1100

    Add timeout while waiting for webserver to start (#233)
    Add timeout while waiting for webserver to start

--- bin/citest.sh
+++ bin/citest.sh
@@ -4,12 +4,6 @@
 # EXPECTS THE SITE TO HAVE ALREADY BEEN BUILT AND RUNNING LOCALLY
 #
 
-echo "Waiting for webserver to start..."
-until $(curl --output /dev/null --silent --head --fail http://localhost:4000); do
-    sleep 5
-done
-echo "Webserver has started"
-
 # Exit immediately if there is an error
 set -e
 
@@ -18,9 +12,6 @@ set -o pipefail
 
 # echo out each line of the shell as it executes
 set -x
-
-#Run pa11y accessibility tests against the local running copy
-node_modules/.bin/pa11y-ci --sitemap http://localhost:4000/pa11y-sitemap.xml
 
 # Run jekyll hyde
 # Note - this will clobber the sitemap.xml using the site.url, so we just use production's
@@ -35,3 +26,18 @@ bundle exec htmlproofer _site  \
     --file-ignore /.*feed/index\.html/ \
     --empty-alt-ignore
 
+echo "Waiting for webserver to start..."
+COUNT=0
+until $(curl --output /dev/null --silent --head --fail http://localhost:4000); do
+    ((COUNT++))
+    if [ "$COUNT" -gt 24 ]
+    then
+        echo "Webserver did not start"
+        exit 1
+    fi
+    sleep 5
+done
+echo "Webserver has started"
+
+#Run pa11y accessibility tests against the local running copy
+node_modules/.bin/pa11y-ci --sitemap http://localhost:4000/pa11y-sitemap.xml
