42272fb135c774f9a68ef7240138f0043aa609f7
Brendan Gordon <brendan.gordon@digital.gov.au>
2017-04-11 16:29:35 +1000

    Add accessibilility test script

---
+++ bin/checkaccessibility.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Checks accessibility.
+
+echo "Running jekyll serve"
+bundle exec jekyll serve --detach --no-watch --destination $TMPDIR/_pa11y_site > /dev/null 2>&1
+
+# Run pa11y accessibility tests
+$(npm bin)/pa11y-ci --sitemap http://localhost:4000/pa11y-sitemap.xml
+
+# kill jekyll
+pkill -f jekyll
