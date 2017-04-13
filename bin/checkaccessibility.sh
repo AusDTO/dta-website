7b7bd9ad7efcaf403540fbb9b16f8e6a7d67c896
Brendan Gordon <brendan.gordon@digital.gov.au>
2017-04-13 10:46:09 +1000

    Use pa11y --sitemap-exclude to ignore pdfs

--- bin/checkaccessibility.sh
+++ bin/checkaccessibility.sh
@@ -6,7 +6,7 @@ echo "Running jekyll serve"
 bundle exec jekyll serve --detach --no-watch --destination $TMPDIR/_pa11y_site > /dev/null 2>&1
 
 # Run pa11y accessibility tests
-$(npm bin)/pa11y-ci --sitemap http://localhost:4000/pa11y-sitemap.xml
+$(npm bin)/pa11y-ci --sitemap http://localhost:4000/sitemap.xml --sitemap-exclude "*.\.pdf"
 
 # kill jekyll
 pkill -f jekyll
