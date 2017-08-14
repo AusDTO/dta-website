e8c0571268670b2aca844535b7d1a3ff9c1c86c8
Jesse Boyd <jesse.boyd@digital.gov.au>
2017-08-14 13:06:51 +1000

    Removed preventDefault().

--- _assets/js/event-tracking.js
+++ _assets/js/event-tracking.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var extensionList = ['.pdf','.doc','.docx','.xls','.xslx','.rtf','.mp4','.srt','.ppt','.pptx'];
   $.each(extensionList, function(index, extension) {
     $('a[href$="' + extension + '"]').click(function(e) {
-        e.preventDefault();
         var pathName = e.currentTarget.pathname;
         var eventCategory = pathName.substr(pathName.lastIndexOf('.') +1).toUpperCase();
         e.currentTarget.title ? eventLabel = e.currentTarget.title : eventLabel = decodeURI(pathName.substr(pathName.lastIndexOf('/') +1));
